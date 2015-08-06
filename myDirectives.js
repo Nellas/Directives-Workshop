0efcf2691637bc22fc33343633b7b694cdb60883
Luke Engelhardt <lukestir@gmail.com>
2015-08-06 09:46:57 -0600

    saving work

---
+++ myDirectives.js
@@ -0,0 +1,10 @@
+var app = angular.module('directiveWorkshop').directive('pending', function() {
+    return {
+        restrict: 'AE',
+        scope: {},
+        link: function(scope, elem, attrs) {
+
+
+        }
+    }
+});
