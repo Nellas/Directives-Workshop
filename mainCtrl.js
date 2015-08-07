4b700469b3b446fafcff9cbfd638d2db7b48e6ac
Luke Engelhardt <luke@securitymetrics.com>
2015-08-07 08:43:58 -0600

    saving work

--- mainCtrl.js
+++ mainCtrl.js
@@ -4,12 +4,5 @@ app.controller('mainCtrl', function($scope, mainService){
 
     $scope.getData = function () {
         return mainService.getData($scope.query);
-        //    .then(function (data) {
-        //   console.log(data);
-        //   return $scope.data = data;
-        //});
     };
-
-
-
 });
--- myDirectives.js
+++ myDirectives.js
@@ -1,11 +1,11 @@
 var app = angular.module('directiveWorkshop').directive('pending', function() {
     return {
-        restrict: 'A',
+        restrict: 'AE',
         scope: {
             request: "&"
 
         },
-        template:'<button ng-hide="loading"> Submit </button> <img style="margin-left: 25px;" src="http://www.ajaxload.info/cache/FF/FF/FF/00/00/00/2-0.gif" ng-show="loading" />',
+        template:'<button ng-hide="loading"> Submit </button> <img style="margin-left: 25px;" src="images/ajax-loader.gif" ng-show="loading" />',
         link: function(scope, elem, attrs) {
 
             elem.on('click', function () {
