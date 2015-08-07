2ae382c4c50c156f49be891df0b8e437cf348db9
Luke Engelhardt <lukestir@gmail.com>
2015-08-07 07:36:24 -0600

    saving work

--- myDirectives.js
+++ myDirectives.js
@@ -2,14 +2,22 @@ var app = angular.module('directiveWorkshop').directive('pending', function() {
     return {
         restrict: 'A',
         scope: {
-            request: '&'
+            request: "&"
 
         },
+        template:'<button ng-hide="loading"> Submit </button> <img style="margin-left: 25px;" src="http://www.ajaxload.info/cache/FF/FF/FF/00/00/00/2-0.gif" ng-show="loading" />',
         link: function(scope, elem, attrs) {
-            //elem.on('click', function () {
-            //    elem.css({display: 'None'});
-            //
-            //})
+
+            elem.on('click', function () {
+                scope.loading = true;
+                scope.request()
+                    .then(function(data) {
+                        console.log(data);
+                        return data;
+                    }).then(function() {
+                        scope.loading = false;
+                    })
+            });
         }
     }
 });
